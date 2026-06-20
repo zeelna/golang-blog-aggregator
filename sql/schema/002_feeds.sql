09cdcce5b54d160013841b268478b102f13ea87e
zeelna <143965319+zeelna@users.noreply.github.com>
2026-06-20 10:55:19 +0300

    added 'addfeeds' command feature

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,16 @@
+-- +goose up
+CREATE TABLE feeds(
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    user_id UUID NOT NULL,
+    CONSTRAINT no_orphaned_feeds
+          FOREIGN KEY (user_id)
+          REFERENCES users(id)
+          ON DELETE CASCADE
+);
+
+-- +goose down
+DROP TABLE feeds;
